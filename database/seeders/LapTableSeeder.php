51ef67cd6ebd7a68c6fe495ab37c225c6da3b818
Karw1n <akbbalfour@gmail.com>
2023-10-31 13:19:08 +0000

    Added lap factory and seeder.

--- database/seeders/LapTableSeeder.php
+++ database/seeders/LapTableSeeder.php
@@ -4,19 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\PlayerProfile;
 
-class PlayerProfileSeeder extends Seeder
+class LapTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $x = new PlayerProfile;
-        $x->email = 'newemail@fakeemail.com';
-        $x->player_id = 1;
-        $x->save();
-        
+        //
     }
 }
