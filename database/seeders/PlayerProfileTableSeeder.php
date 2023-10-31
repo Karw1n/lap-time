95b268c486b8889ea8b3ae45daaa1ff99fca90e9
Karw1n <akbbalfour@gmail.com>
2023-10-31 13:17:38 +0000

    Renamed PlayerProfileSeeder

---
+++ database/seeders/PlayerProfileTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\PlayerProfile;
+
+class PlayerProfileTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $x = new PlayerProfile;
+        $x->email = 'newemail@fakeemail.com';
+        $x->player_id = 1;
+        $x->save();
+        
+    }
+}
