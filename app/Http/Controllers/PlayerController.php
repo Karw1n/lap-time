6291ed47d40c4572c5c89faa2a50199e221bcd24
Karw1n <akbbalfour@gmail.com>
2023-11-16 16:15:41 +0000

    Added use statement for Player to call Player.

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Data\Player;
 
 class PlayerController extends Controller
 {
@@ -12,6 +13,7 @@ class PlayerController extends Controller
     public function index()
     {
         //
+        $players = Player::all();
         return view('players.index');
     }
 
