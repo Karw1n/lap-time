27b76503212ab38696d78f93e9fe8a449bb0e986
Karw1n <akbbalfour@gmail.com>
2023-11-19 15:54:12 +0000

    Added players.create route.

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ use App\Http\Controllers\PlayerController;
 
 Route::get('/players', [PlayerController::class, 'index'])->name('players.index');
 
+Route::get('/players/create', [PlayerController::class, 'create'])->name('players.create');
+
 Route::get('/players/{id}', [PlayerController::class, 'show'])->name('players.show');
 
 Route::get('/', function () {
