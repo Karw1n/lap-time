3b8040d00e0b9353389bce119a626f5037edd6fd
Karw1n <akbbalfour@gmail.com>
2023-11-19 16:23:53 +0000

    Created Player Form.

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -22,7 +22,7 @@ class PlayerController extends Controller
      */
     public function create()
     {
-        //
+        return view('players.create');
     }
 
     /**
---
+++ resources/views/players/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('title', 'Player Creator')
+
+<body>
+    <h1> Best Laps - @yield('title')</h1>
+
+@section('content')
+
+<form method="POST" action = "{{route('players.store')}}">
+    @csrf
+    <ul>
+        <li>Name: <input type="text" name="name"/></li>
+        <li>Favourite Team: <input type="text" name="favourite_team"/></li>
+        <input type="submit" value="Submit"/>
+    </ul>
+    <a href="{{route('players.index')}}">Cancel</a>
+</form>
+@endsection
+
+</body>
