4dfc14c42b1ca0a3cfb3c6e81776bae4e67e47d5
Karw1n <akbbalfour@gmail.com>
2023-12-04 19:10:58 +0000

    Added and implemented posts factory.

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -13,23 +13,6 @@ class PostsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $posts = [
-            [
-                'title' => 'First Post',
-                'excerpt' => 'Summary of the First Post',
-                'body' => 'Body of First Post',
-                'image_path' => 'Empty',
-            ],
-            [
-                'title' => 'Second Post',
-                'excerpt' => 'Summary of the Second Post',
-                'body' => 'Body of Second Post',
-                'image_path' => 'Empty',
-            ]
-        ];           
-        
-        foreach($posts as $key => $value) {
-            Post::create($value);
-        } 
+        Post::factory(10)->create();        
     }
 }
