e2e53b718461121d8b3cbaf7364b7681177a1206
Karw1n <akbbalfour@gmail.com>
2023-12-05 11:10:34 +0000

    Created index for Post that displays all titles.

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,3 +1,15 @@
-<h1>
-    Index Method of Blogs
-</h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+</head>
+<body>
+@foreach ($posts as $post)
+            <li>
+                {{$post->title}}
+            </li>
+        @endforeach 
+</body>
+</html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,13 @@
-<h1>
-    Home Page
-</h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+</head>
+<body>
+    <h1 class="text=3xl p-2 font-bold">
+        Tailwind 3.0
+    </h1>  
+</body>
+</html>
