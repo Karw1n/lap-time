17c55bbf65d0cce7c5a995261e630693b0a041aa
Karw1n <akbbalfour@gmail.com>
2023-12-05 21:33:04 +0000

    Defined foreign relationship in post model.

--- database/migrations/2023_12_04_165056_create_posts_table.php
+++ database/migrations/2023_12_04_165056_create_posts_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->string('title')->unique();
             $table->text('excerpt');
             $table->text('body');
             $table->string('image_path')->nullable();
+            
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
     }
