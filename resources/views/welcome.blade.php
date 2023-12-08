dc99f557ee5af59371ba94d7bca4b29ad8c1bd04
Karw1n <akbbalfour@gmail.com>
2023-12-08 17:51:29 +0000

    Added a welcome page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="flex items-center justify-center h-screen">
 <div class="w-2/3 bg-white p-8 rounded shadow-md ">
   <div class="text-center">
-    <h1 class="text-4xl font-bold mb-4 text-blue-700">Welcome to my Blog Application</h1>
+    <h1 class="text-4xl font-bold mb-4 text-gray-800">Welcome to my Blog Application</h1>
     <p class="text-gray-600">Explore and share your thoughts with the world.</p>
   </div>
 </div>
