8ca7a58098b8629bb06035d9525467a3fc2bfba6
Karw1n <akbbalfour@gmail.com>
2023-12-08 17:46:10 +0000

    Created a seeder for users to create admin.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User;
+        $user->name = 'admin';
+        $user->email = 'admin@admin.com';
+        $user->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
+        $user->remember_token = Str::random(10);
+        $user->role_as = '1';
+        $user->save();
+        
+    }
+}
